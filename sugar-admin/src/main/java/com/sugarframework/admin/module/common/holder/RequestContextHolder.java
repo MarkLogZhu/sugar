37e3696957565bfbfa1f379aeffc331ffafd5327
MarkLogZhu <markLogZhu@163.com>
2019-06-24 15:51:39 +0800

    请求上下文容器增加 get 方法

--- sugar-admin/src/main/java/com/sugarframework/admin/module/common/holder/RequestContextHolder.java
+++ sugar-admin/src/main/java/com/sugarframework/admin/module/common/holder/RequestContextHolder.java
@@ -5,25 +5,34 @@ import com.sugarframework.admin.module.common.model.UserDto;
 import javax.servlet.http.HttpServletRequest;
 
 /**
-  * @description: 请求上下文容器
-  * @author zhu
-  * @date 2019-06-24 14:59
-  */
+ * @author zhu
+ * @description: 请求上下文容器
+ * @date 2019-06-24 14:59
+ */
 public class RequestContextHolder {
 
     private static final ThreadLocal<HttpServletRequest> REQUEST_THREAD_LOCAL = new ThreadLocal<>();
     private static final ThreadLocal<UserDto> USER_THREAD_LOCAL = new ThreadLocal<>();
 
 
-    public static void add(HttpServletRequest request){
+    public static void add(HttpServletRequest request) {
         REQUEST_THREAD_LOCAL.set(request);
     }
 
-    public static void add(UserDto userDto){
+    public static UserDto getCurrentUser() {
+        return USER_THREAD_LOCAL.get();
+    }
+
+    public static void add(UserDto userDto) {
         USER_THREAD_LOCAL.set(userDto);
     }
 
-    public static void remove(){
+    public static HttpServletRequest getCurrentRequest() {
+        return REQUEST_THREAD_LOCAL.get();
+    }
+
+
+    public static void remove() {
         REQUEST_THREAD_LOCAL.remove();
         USER_THREAD_LOCAL.remove();
     }
