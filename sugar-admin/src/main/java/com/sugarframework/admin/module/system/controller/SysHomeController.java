9935beca891b790b326225635ec534ad85a14f93
MarkLogZhu <markLogZhu@163.com>
2019-07-23 14:38:27 +0800

    清理多余导入包

--- sugar-admin/src/main/java/com/sugarframework/admin/module/system/controller/SysHomeController.java
+++ sugar-admin/src/main/java/com/sugarframework/admin/module/system/controller/SysHomeController.java
@@ -1,7 +1,6 @@
 package com.sugarframework.admin.module.system.controller;
 
 import com.sugarframework.admin.module.common.holder.RequestContextHolder;
-import com.sugarframework.admin.module.common.model.MenuDto;
 import com.sugarframework.admin.module.common.model.UserDto;
 import com.sugarframework.admin.module.system.service.SysMenuManagementService;
 import com.sugarframework.core.base.controller.BaseController;
@@ -9,9 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
  * @author zhu
